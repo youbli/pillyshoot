374007fae2182855fbdafa63a93a90a4809fa053
racoon0425 <sjrnf0425@naver.com>
2021-09-26 02:51:59 +0900

    video

--- camera.js
+++ camera.js
@@ -1,2 +1,14 @@
-$(document).ready(function () { if (!('url' in window) && ('webkitURL' in window)) { window.URL = window.webkitURL; } $('#camera').change(function (e) { $('#pic').attr('src', URL.createObjectURL(e.target.files[0])); }); });
+let canvas = document.querySelector("#canvas");
+let context = canvas.getContext("2d");
+let video = document.querySelector("#video");
 
+if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
+        video.srcObject = stream;
+        video.play();
+    });
+}
+
+document.getElementById('snap').addEventListener('click', () => {
+    context.drawImage(video, 0, 0, 640, 480);
+})
